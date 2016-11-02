bee6c085350d355e8b00a1eea0e246280b4254bc
kap9i <kap9i@mail.ru>
2016-11-02 18:06:21 +0300

    Add files via upload

---
+++ 03_guitar/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $('.menu_toggle').click(function () {
+        $('nav ul').slideToggle(500);
+    });
+
+    $(function externalLinks() {
+
+        for (i = 0; i < $('a').length; i++) {
+            link = $('a')[i];
+            if (link.getAttribute("href") && link.getAttribute("rel") == "external")
+                link.target = "_blank";
+        }
+    });
+
+});
