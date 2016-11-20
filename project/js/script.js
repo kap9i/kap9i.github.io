a4d48e62ba80c9904b2ce84b5b5d3df603e622fe
kap9i <kap9i@mail.ru>
2016-11-20 17:36:17 +0300

    Add files via upload

--- project/js/script.js
+++ project/js/script.js
@@ -2,9 +2,22 @@ $(function () {
 
     images = $('.choice__form');
     form = $('.jc_subblock_1');
+    cls = $('.choice__close');
+    body = $('body');
+    hidden = $('.hide-layout');
 
     images.click(function () {
         form.css({display: 'block'}, 500)
             .animate({opacity: 1}, 500)
+
+        hidden.css({background: '#000', opacity: 0.5, width: '100%', height: '100%', bottom: 0, top: 0, position: 'fixed'})
+
+    });
+
+    cls.click(function () {
+        form.css({display: 'none'}, 500)
+            .animate({opacity: 0}, 500)
+
+        hidden.css({opacity: 0, width: '0', height: '0'})
     });
 });
