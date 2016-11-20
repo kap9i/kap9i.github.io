7987343597a911234af0f765266a802dc8d31fe7
kap9i <kap9i@mail.ru>
2016-11-20 12:31:08 +0300

    Add files via upload

--- project/js/script.js
+++ project/js/script.js
@@ -7,9 +7,4 @@ $(function () {
         form.css({display: 'block'}, 500)
             .animate({opacity: 1}, 500)
     });
-
-    form.click(function () {
-        form.css({display: 'none'}, 500)
-            .animate({opacity: 0}, 500)
-    });
 });
