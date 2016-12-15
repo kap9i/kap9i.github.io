db980c27460300de993e7e43621ee47b3cf10f0d
kap9i <kap9i@mail.ru>
2016-12-15 20:03:27 +0300

    delete blog.rar

--- theme-axxon/sidebar.php
+++
@@ -1,7 +0,0 @@
-<div class="col-lg-4 col-md-4">
-    <?php if (!dynamic_sidebar('sidebar')): ?>
-        <div class="sidebar_my">
-            <h3>Виджеты сайдбара</h3>
-        </div>
-    <?php endif; ?>
-</div>
